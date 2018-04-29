f7ea15333d801d2cdf86e0d729b7b79898522938
Dafna Ehrman <dafna972@gmail.com>
2018-04-29 14:32:21 +0300

    authentication bug
    user changed from Dafna (admin) to regular user

--- mongo.js
+++ mongo.js
@@ -1,6 +1,6 @@
 const debug = require('debug')('mongo:mongo')
 const mongo = require("mongoose");
-let db = mongo.createConnection('mongodb://Dafna:Dafna972_@yochidafna-shard-00-00-zemea.mongodb.net:27017,yochidafna-shard-00-01-zemea.mongodb.net:27017,yochidafna-shard-00-02-zemea.mongodb.net:27017/test?ssl=true&replicaSet=YochiDafna-shard-0&authSource=admin');
+let db = mongo.createConnection('mongodb://user:user@yochidafna-shard-00-00-zemea.mongodb.net:27017,yochidafna-shard-00-01-zemea.mongodb.net:27017,yochidafna-shard-00-02-zemea.mongodb.net:27017/test?ssl=true&replicaSet=YochiDafna-shard-0&authSource=admin');
 db.on('open', function() { debug("On open DB") });
 db.on('error', function(err) {  debug("Error connecting to DB" + err) });
 db.on('connecting', () => { debug('On connecting to MongoDB: '); });
