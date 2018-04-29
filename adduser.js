fce6b72d1775c9ecd7540095a2ce6bf5263a902b
Dafna Ehrman <dafna972@gmail.com>
2018-04-29 21:19:26 +0300

    asd
    asd

--- adduser.js
+++ adduser.js
@@ -1,6 +1,6 @@
 const User = require('./model')("User");
-const prompt = require('./prompt');
-const timeout = require('./timeout');
+const prompt = require('prompt');
+const timeout = require('timeout');
 (async () => {
     console.clear();
     await timeout(500);
